5405bb833778a4208395eefd40d061796d5ec841
unknown <beatrizsilva.sms@gmail.com>
2021-12-07 12:00:59 -0300

    template modelo base adicionado

--- CRUD/Pages/views.py
+++ CRUD/Pages/views.py
@@ -5,4 +5,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class IndexView(TemplateView):
-    template_name = 'Pages/index.html'
+    #template_namme = index.html
+    #template_name = 'Pages/index.html'
+    template_name = 'Pages/modelo.html'
