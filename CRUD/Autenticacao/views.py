cea109aa94cc4953b7a68b7c6d2a796f1df0ea06
unknown <beatrizsilva.sms@gmail.com>
2021-12-08 19:36:54 -0300

    add cadastro de usuários

--- CRUD/Autenticacao/views.py
+++ CRUD/Autenticacao/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.db.models import fields
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UsuarioForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+class UsuarioCreate(CreateView):
+    template_name= "autenticacao/cadastroUser.html"
+    form_class = UsuarioForm
+    #fields = ['username', 'email', 'password']
+    success_url = reverse_lazy('login')
+
+#fhZTU63JYRc_gx8
