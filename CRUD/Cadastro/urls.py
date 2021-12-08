12d7d5c4a828f089282607d1af89f312906f5197
unknown <beatrizsilva.sms@gmail.com>
2021-12-08 10:22:44 -0300

    READ Local(Gênero)

--- CRUD/Cadastro/urls.py
+++ CRUD/Cadastro/urls.py
@@ -4,17 +4,24 @@ from .views import CadastroView
 from .views import LivroCreat, LocalCreat
 from .views import LivroUpdate, LocalUpdate
 from .views import LivroDelete, LocalDelete
+from .views import LivroList, LocalList
 
 urlpatterns = [
-    #Cadastro - form.html
+    #CREAT - form.html 
     path('cadastro/', CadastroView.as_view(), name='cadastro'),  
     path('cadastro/local/', LocalCreat.as_view(), name='cadastro-local'),
     path('cadastro/livro/', LivroCreat.as_view(), name='cadastro-livro'),
 
+    #UPDATE
     path('editar/local/<str:pk>/', LocalUpdate.as_view(), name='editar-local'),
     path('editar/livro/<int:pk>/', LivroUpdate.as_view(), name='editar-livro'),
 
+    #DELETE
     path('excluir/local/<str:pk>/', LocalDelete.as_view(), name='excluir-local'),
     path('excluir/livro/<int:pk>/', LivroDelete.as_view(), name='excluir-livro'),
+
+    #READ
+    path('listar/local/', LocalList.as_view(), name='listar-local'),
+    path('listar/livro/', LivroList.as_view(), name='listar-livro'),
 
 ]
--- CRUD/Cadastro/views.py
+++ CRUD/Cadastro/views.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 from .models import Livro, Local
 from django.urls import reverse_lazy
 
@@ -14,7 +15,7 @@ class LocalCreat(CreateView):
     model = Local
     fields = ['codigo', 'descricao', 'corredor', 'estante']
     template_name = 'Cadastro/form.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('listar-local')
 
 class LivroCreat(CreateView):
     model = Livro
@@ -28,7 +29,7 @@ class LocalUpdate(UpdateView):
     model = Local
     fields = ['codigo', 'descricao', 'corredor', 'estante']
     template_name = 'Cadastro/form.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('listar-local')
 
 class LivroUpdate(UpdateView):
     model = Livro
@@ -40,10 +41,19 @@ class LivroUpdate(UpdateView):
 ################## DELETE ########################
 class LocalDelete(DeleteView):
     model = Local
-    template_name = 'Cadastro/tela-excluir.html'
+    template_name = 'Cadastro/excluir.html'
     success_url = reverse_lazy('home')
 
 class LivroDelete(DeleteView):
     model = Livro
-    template_name = 'Cadastro/tela-excluir.html'
-    success_url = reverse_lazy('home')
+    template_name = 'Cadastro/excluir.html'
+    success_url = reverse_lazy('listar-local')
+
+################## READ ########################
+class LocalList(ListView):
+    model = Local
+    template_name = 'Cadastro/listaLocal.html'
+
+class LivroList(ListView):
+    model = Livro
+    template_name = 'Cadastro/listaLivro.html'
