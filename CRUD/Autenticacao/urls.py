9d4a37067d0faeca715b1d55744fd7cfe2d852a4
unknown <beatrizsilva.sms@gmail.com>
2021-12-08 14:48:07 -0300

    template autenticacao funcionando

--- CRUD/Autenticacao/urls.py
+++ CRUD/Autenticacao/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-urlpatterns = {
-    #path('', view, name=""),
+urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
-        template_name = 'autenticacao/login.html'
+        template_name='autenticacao/login.html'
     ), name="login"),
-
-}
+]
--- CRUD/CRUD/settings.py
+++ CRUD/CRUD/settings.py
@@ -137,3 +137,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Autenticação
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
